7ac6d0e1c1c6d0e0c35a54aa61f4e1f9cf6adc2c
Reid Spreiter <reidspreiter@gmail.com>
2025-11-15 09:33:51 -0600

    Core/State/SPGameState: add and setup/teardown managers

--- SkyPlexC2/Source/SkyPlexC2/Public/Core/State/SPGameState.h
+++ SkyPlexC2/Source/SkyPlexC2/Public/Core/State/SPGameState.h
@@ -15,6 +15,75 @@ class SKYPLEXC2_API ASPGameState : public AGameStateBase
 	GENERATED_BODY()
 	
 public:
+	// Object types to spawn to support blueprint defined managers
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPLogger> LoggerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPPreferences> PreferencesManagerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPAuthManager> AuthManagerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPWorldManager> WorldManagerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPDatabaseManager> DatabaseManagerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPFlyToManager> FlyToManagerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPDroneManager> DroneManagerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPMissionManager> MissionManagerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPObstacleManager> ObstacleManagerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPSimulationManager> SimulationManagerToSpawn;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	TSubclassOf<class USPSelectionManager> SelectionManagerToSpawn;
+
+
+	// Manager references and other common objects
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPLogger> Logger;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPPreferences> Preferences;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPAuthManager> AuthManager;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPWorldManager> WorldManager;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPDatabaseManager> DatabaseManager;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPFlyToManager> FlyToManager;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPDroneManager> DroneManager;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPMissionManager> MissionManager;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPObstacleManager> ObstacleManager;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPSimulationManager> SimulationManager;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	TObjectPtr<USPSelectionManager> SelectionManager;
+
 	UPROPERTY(BlueprintReadOnly)
 	TObjectPtr<class ACesium3DTileset> CesiumTileset;
 
@@ -24,9 +93,12 @@ public:
 	UPROPERTY(BlueprintReadOnly)
 	TObjectPtr<UWorld> SPWorld;
 
+	ASPGameState();
+
 	UFUNCTION(BlueprintCallable, Category = "SPGameState", meta = (WorldContext = "WorldContextObject"))
 	static ASPGameState* GetSPGameState(const UObject* WorldContextObject);
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 };
