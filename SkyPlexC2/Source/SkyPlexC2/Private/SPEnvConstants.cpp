b992b1d1544f7bdb5f409fe3e5a4fc1dabaaa246
Reid Spreiter <reidspreiter@gmail.com>
2025-11-16 21:10:01 -0600

    SPEnvConstants: include Google urls, database and fly to paths

--- SkyPlexC2/Source/SkyPlexC2/Private/SPEnvConstants.cpp
+++ SkyPlexC2/Source/SkyPlexC2/Private/SPEnvConstants.cpp
@@ -17,3 +17,16 @@ FString USPEnvConstants::GetPreferencesPath() {
 	static FString PreferencesPath = GetUserSavePath() / TEXT("preferences.json");
 	return PreferencesPath;
 }
+
+FString USPEnvConstants::GetFlyToLocationPath() {
+	static FString FlyToLocationPath = GetUserSavePath() / TEXT("flyToLocations.json");
+	return FlyToLocationPath;
+}
+
+FString USPEnvConstants::GetDatabasePath() {
+	static FString DatabasePath = GetUserSavePath() / TEXT("skyplexc2.db");
+	return DatabasePath;
+}
+
+const FString USPEnvConstants::FIREBASE_URL = TEXT("https://identitytoolkit.googleapis.com/v1/");
+const FString USPEnvConstants::GOOGLE_TOKEN_URL = TEXT("https://securetoken.googleapis.com/v1/");
