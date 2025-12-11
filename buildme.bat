e5e91a4df547edfccf0310192c83333a9ab806c5
lprosperie <logan@synetos.ai>
2025-12-11 11:06:37 -0600

    Edited buildme script to add the package command from Reid without user-specific paths.

--- buildme.bat
+++ buildme.bat
@@ -1,6 +1,4 @@
-@echo SkyplexC2 build start pretend build
+@echo SkyplexC2 start build
 
-:: pretend test
-powershell -Command "Remove-Item -Path '.\build\' -Recurse -ErrorAction SilentlyContinue"
-mkdir build
-xcopy .\.git* .\build\
+:: This currently implies SkyPlex-C2 is cloned at the user's home directory
+powershell -Command "C:\Program Files\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project='$HOME\skyplexc2\SkyPlexC2\SkyPlexC2.uproject' -platform='Win64' -clientconfig='Shipping' -alltimings -build -cook -stage -package -archive -archivedirectory='$HOME\skyplexc2\Windows' -pak -prereqs -unattended"
