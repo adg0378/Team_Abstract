f339fcc42eb88fa9b496fbcc9d82d438af2d1040
lprosperie <logan@synetos.ai>
2025-12-12 11:26:18 -0600

    Rewrote buildme.bat with line continuation for easier reading and for complying with GitLab Runner path behavior.

--- buildme.bat
+++ buildme.bat
@@ -1,4 +1,13 @@
 @echo SkyplexC2 start build
 
-:: This currently implies SkyPlex-C2 is cloned at the user's home directory
-powershell -Command "C:\Program Files\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project='$HOME\skyplexc2\SkyPlexC2\SkyPlexC2.uproject' -platform='Win64' -clientconfig='Shipping' -alltimings -build -cook -stage -package -archive -archivedirectory='$HOME\skyplexc2\Windows' -pak -prereqs -unattended"
+set UPROJECT=%CI_PROJECT_DIR%\SkyPlexC2\SkyPlexC2.uproject
+set ARCHIVE_DIR=%CI_PROJECT_DIR%\SkyPlexC2\BuildOutput
+
+powershell -Command "C:\Program Files\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun ^
+	-project='%UPROJECT%' ^
+	-platform='Win64' ^
+	-clientconfig='Shipping' ^
+	-alltimings ^
+	-build -cook -stage -package -archive ^
+	-archivedirectory='%ARCHIVE_DIR%' ^
+	-pak -prereqs -unattended"
