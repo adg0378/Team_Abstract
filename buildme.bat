ce8ee08ea0426d99986ff86b791b008758e11cf6
lprosperie <logan@synetos.ai>
2025-12-17 12:29:17 -0600

    Rewrote buildme.bat to not mix Powershell and cmd syntax, as it caused a failure before. Testing the new script with gitlab-runner.

--- buildme.bat
+++ buildme.bat
@@ -1,13 +1,17 @@
 @echo SkyplexC2 start build
+setlocal 
 
 set UPROJECT=%CI_PROJECT_DIR%\SkyPlexC2\SkyPlexC2.uproject
 set ARCHIVE_DIR=%CI_PROJECT_DIR%\SkyPlexC2\BuildOutput
 
-powershell -Command "C:\Program Files\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun ^
-	-project='%UPROJECT%' ^
-	-platform='Win64' ^
-	-clientconfig='Shipping' ^
-	-alltimings ^
-	-build -cook -stage -package -archive ^
-	-archivedirectory='%ARCHIVE_DIR%' ^
-	-pak -prereqs -unattended"
+call "C:\Program Files\Epic Games\UE_5.5\Engine\Build\BatchFiles\RunUAT.bat" ^
+    BuildCookRun ^
+    -project="%UPROJECT%" ^
+    -platform=Win64 ^
+    -clientconfig=Shipping ^
+    -alltimings ^
+    -build -cook -stage -package -archive ^
+    -archivedirectory="%ARCHIVE_DIR%" ^
+    -pak -prereqs -unattended
+	
+endlocal
